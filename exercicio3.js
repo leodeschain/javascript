d15b7a4337ee78a998fdd4ef8ed4b67c20abf481
Leopoldo <leopoldopsn@gmail.com>
2025-07-04 15:00:43 -0300

    diferença entre const e let

---
+++ exercicio3.js
@@ -0,0 +1,11 @@
+//Const e let
+    const numero1 = 1;
+    let numero2 = 2;
+    // Qual a diferença entre const e let?
+    // const é uma constante, não pode ser reatribuída. let pode ser reatribuida    
+
+    if(numero1 === 1) {
+       numero2 = 6;  
+    }
+
+    console.log('resultado', numero2)
