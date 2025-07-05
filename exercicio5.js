55a31f43ae394ffd3569f66769a2a84190c091c8
Leopoldo <leopoldopsn@gmail.com>
2025-07-05 14:34:45 -0300

    Avançando no aprendizado javscript - FOR

--- exercicio5.js
+++ exercicio5.js
@@ -11,3 +11,7 @@ console.log('usuario pode entrar', podeEntrar);
 
 // Este é o primeiro programa em JavaScript que verifica se um usuário pode entrar com base na idade ou no nome.
 //aprendizado sobre as diferentes formas de usar o if, o else, o console.log, revisão de variáveis const e let além de operadores lógicos como || (OU) e && (E)
+
+//obs: uso dos 3 iguais faz javascript interpretar o valor e o tipo, enquanto o uso de apenas 2 iguais faz javascript interpretar apenas o valor, independente do tipo. Exemplo: 1 === '1' é false, enquanto 1 == '1' é true.
+//poderia ser aplicado no programa acima por exemplo com if idade === 18, mas como o valor é numérico, não há necessidade de usar os 3 iguais, poderia ser usado apenas 2 iguais, porém
+//na situação inversa, se fosse com aspas idade === '18', aí sim seria necessário usar os 3 iguais, pois o valor é uma string e não um número, então o tipo é diferente, se não usasse os 3 iguais, o javascript interpretaria como true, pois o valor é o mesmo, mas o tipo é diferente, então seria false, e o usuário não poderia entrar.
---
+++ exercicio6for.js
@@ -0,0 +1,19 @@
+const quantidade = 10;
+
+for(let i = 0; i<quantidade; i++) {
+    console.log(i);
+}
+
+console.log('acabou o loop');
+// Este é o primeiro programa em JavaScript que usa um loop for para imprimir números de 0 a 9 no console.
+// Aprendizado sobre a estrutura do loop for, como inicializar uma variável, definir uma    condição de parada e incrementar a variável.
+// Além disso, o console.log é usado para imprimir os números e uma mensagem final indicando que o loop terminou.
+
+//exemplo com multiplicação:
+//const quantidade = 10;
+//let multiplicacao = 1;
+//for(let i = 0; i<quantidade; i++) {
+//   console.log(i);
+// soma = soma + i;
+//}
+// console.log('Acabou o loop', soma);
