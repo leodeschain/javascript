e65700ac5b872c780b92bfb669610b8faeaa5d31
Leopoldo <leopoldopsn@gmail.com>
2025-07-05 17:45:27 -0300

    diferença entre while do while

---
+++ exercicio09.js
@@ -0,0 +1,9 @@
+let idade = 18;
+
+do {
+    console.log('aprovado', idade);
+    idade++;
+    // A variável idade é incrementada em 1 a cada iteração do loop.    
+    } while (idade < 18);
+// Este é um loop do-while que imprime "aprovado" seguido do valor da variável idade enquanto idade for menor que 18.
+// O loop garante que o bloco de código seja executado pelo menos uma vez, mesmo que
