9bb5754891830e83f3d88a68abf9b32c9d8e5f2f
Leopoldo <leopoldopsn@gmail.com>
2025-07-05 17:27:34 -0300

    Continuando exercícios: while

---
+++ exercicio07.js
@@ -0,0 +1,5 @@
+while(true) {
+    console.log("Continua a execução do programa");
+}
+// Este é um loop infinito que imprime "Continua a execução do programa" no console.
+// Para interromper a execução, você pode usar Ctrl+C no terminal ou fechar o navegador se  estiver rodando em um ambiente de navegador.
---
+++ exercicio08.js
@@ -0,0 +1,8 @@
+//Mesmo exemplo do exercício 07 mas usando idade como limite para o loop
+// Este exemplo usa uma variável idade para controlar o loop, que continua enquanto a idade for menor
+let idade = 1;
+
+while (idade < 18) {
+    console.log('continua', idade);
+    idade = idade + 1;
+}
