60e57fefc051abc100f721a3626046f57ebbe6da
Leopoldo <leopoldopsn@gmail.com>
2025-07-06 11:17:17 -0300

    criando minha primeira função

---
+++ exercicio12.js
@@ -0,0 +1,9 @@
+function soma(item1,item2) {
+return item1 + item2;
+}
+
+console.log('SOMA:', soma(5,6));
+// Este é um exemplo de uma função JavaScript que recebe dois parâmetros (item1 e item2) e retorna a soma deles.
+// A função é chamada com os argumentos 5 e 6, e o resultado da soma (11) é impresso no console.
+console.log('SOMA:', soma(5612,6578));
+//poderia ser usado operação de subtração por exemplo e chamando de diminuir ao invés de soma
