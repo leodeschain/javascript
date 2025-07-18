b1f9f701251c2279ce8085a56622aab12f5b93e6
Leopoldo <leopoldopsn@gmail.com>
2025-07-17 22:09:06 -0300

    outra forma de exibir o objeto usando $

--- funcoes_e_objetos/exercicio7.js
+++ funcoes_e_objetos/exercicio7.js
@@ -21,3 +21,14 @@ exibirInformacoes(joaquim);
 
 //ao inves de passar o objeto inteiro, podemos passar apenas as propriedades que queremos exibir
 // ao inves de colocar no script 'nome, idade, peso, genero, nacionalidade, endereco, numero, pais, temFilhos', podemos passar apenas as propriedades que queremos exibir
+//para acessar dados especificos do objeto, console.log('Nome',pessoa.nome); etc, daria pra colocar tudo na 'exibirInformacoes' e ao inves de passar o objeto inteiro, podemos passar apenas as propriedades que queremos exibir
+
+//também podemos fazer
+
+const exibirInformacao2 = (pessoa, qualInformacao) => {
+    console.log(`${qualInformacao} do(a) ${pessoa.nome} é: ${pessoa[qualInformacao]}`);
+}
+exibirInformacao2(joaquim, 'nome');
+exibirInformacao2(joaquim, 'idade');
+exibirInformacao2(joaquim, 'peso');
+
